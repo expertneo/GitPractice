6192b73b64cb2848ffff6433cf4e4a033839a003
Adamka <yehveeen@gmail.com>
2018-02-15 14:43:47 +0100

    b5

--- src/DummyClass.java
+++ src/DummyClass.java
@@ -2,14 +2,15 @@ public class DummyClass {
 
   public static void main(String[] args) {
     String name = "Greetings!";
-    int age = 25;
+    int age = 1992;
     String month = "June";
+    String day = "21";
     String myName = "Java";
     String horoscope = "Gemini";
     String chineseHoroscope = "Horse";
     cheer(myName);
     greet(name);
-    count(age, month);
+    count(age, month, day);
     future(horoscope, chineseHoroscope);
   }
 
@@ -21,8 +22,8 @@ public class DummyClass {
     System.out.println(name);
   }
 
-  public static void count(int age, String month) {
-    System.out.println("My age is: " + age + " " + month);
+  public static void count(int age, String month, String day) {
+    System.out.println("My birthday is : " + age + " " + month + " " + day);
   }
 
   public static void future(String horoscope, String chineseHoroscope) {
