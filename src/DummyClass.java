e1517be7bda4c7448dd3309382dd7190a369728b
Bence Papp <bence.papp.hu@gmail.com>
2018-02-15 13:21:53 +0100

    a1

--- src/DummyClass.java
+++ src/DummyClass.java
@@ -1,2 +1,10 @@
 public class DummyClass {
+  public static void main(String[] args) {
+    String name = "Bence";
+    greet(name);
+  }
+
+  public static void greet(String name) {
+    System.out.println(name);
+  }
 }
